d2a76ec6e65b7b14191f54c1e477a73562cbf1cf
kenken1130 <kenken1130@users.noreply.github.com>
2026-02-28 13:48:49 +0900

    Add dedicated fix script for Issue #7: Vertical Spacing Compression

---
+++ Assets/Code/Scripts/Editor/HomeScreenSpacingFix.cs
@@ -0,0 +1,25 @@
+using UnityEngine;  
+using UnityEditor;  
+  
+public class HomeScreenSpacingFix  
+{ 
+    [MenuItem(\"Tools/GOLFIN/Fix Issue #7 - Vertical Spacing\")]  
+    public static void FixVerticalSpacing()  
+    {  
+        Debug.Log(\"[GOLFIN] Looking for HomeScreen objects...\");  
+        HomeScreen[] homeScreens = FindObjectsOfType<HomeScreen>();  
+        foreach (var homeScreen in homeScreens)  
+        {  
+            Transform nextHole = homeScreen.transform.Find(\"NextHolePanel\");  
+            if (nextHole != null)  
+            {  
+                RectTransform rt = nextHole.GetComponent<RectTransform>();  
+                Vector2 pos = rt.anchoredPosition;  
+                pos.y -= 20f;  
+                rt.anchoredPosition = pos;  
+                EditorUtility.SetDirty(nextHole.gameObject);  
+                Debug.Log(\"[GOLFIN] Fixed spacing for \" + homeScreen.name);  
+            }  
+        }  
+    }  
+} 
